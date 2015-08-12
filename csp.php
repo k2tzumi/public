da9e2ba8539159603d7dd580e533dfef28a7a053
Carlos C <uldericofilho@gmail.com>
2015-08-12 10:51:41 +0200

    Putting signal handle in the correct place

--- csp.php
+++ csp.php
@@ -21,16 +21,17 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-define('PHP_INT_LENGTH', strlen(sprintf("%u", PHP_INT_MAX)));
+define('PHP_INT_LENGTH', strlen(sprintf('%u', PHP_INT_MAX)));
 function cofunc(callable $fn) {
 	$pid = pcntl_fork();
 	if (-1 == $pid) {
 		trigger_error('could not fork', E_ERROR);
-	}
-	if ($pid) {
-		pcntl_signal(SIGCHLD, SIG_IGN);
 		return;
 	}
+	if ($pid) {
+		return;
+	}
+	pcntl_signal(SIGCHLD, SIG_IGN);
 	$params = [];
 	if (func_num_args() > 1) {
 		$params = array_slice(func_get_args(), 1);
@@ -55,9 +56,11 @@ class CSP_Channel {
 		]);
 
 	}
+
 	public function msg_count() {
 		return $this->msg_count;
 	}
+
 	public function close() {
 		$this->closed = true;
 		do {
@@ -67,6 +70,7 @@ class CSP_Channel {
 		msg_remove_queue($this->ipc);
 		file_exists($this->ipc_fn) && @unlink($this->ipc_fn);
 	}
+
 	public function in($msg) {
 		if ($this->closed || !msg_queue_exists($this->key)) {
 			return;
@@ -77,6 +81,7 @@ class CSP_Channel {
 		@msg_send($this->ipc, 1, $shm->key(), false, true, $error);
 		++$this->msg_count;
 	}
+
 	public function non_blocking_in($msg) {
 		if ($this->closed || !msg_queue_exists($this->key)) {
 			return self::CLOSED;
@@ -101,6 +106,7 @@ class CSP_Channel {
 		} while (true);
 		return true;
 	}
+
 	public function out() {
 		if ($this->closed || !msg_queue_exists($this->key)) {
 			return;
@@ -114,6 +120,7 @@ class CSP_Channel {
 		$ret = $shm->fetch();
 		return $ret;
 	}
+
 	public function non_blocking_out() {
 		if ($this->closed || !msg_queue_exists($this->key)) {
 			return [self::CLOSED, null];
@@ -144,19 +151,23 @@ class Message {
 		}
 		$this->key = $key;
 	}
+
 	public function store($msg) {
 		shm_put_var($this->shm, 1, $msg);
 		shm_detach($this->shm);
 	}
+
 	public function key() {
 		return sprintf('%0' . PHP_INT_LENGTH . 'd', (int) $this->key);
 	}
+
 	public function fetch() {
 		$ret = shm_get_var($this->shm, 1);
 		$this->destroy();
 		return $ret;
 
 	}
+
 	public function destroy() {
 		if (shm_has_var($this->shm, 1)) {
 			shm_remove_var($this->shm, 1);
