5748c32013c86b64b9e89b65c2bebdeaedcc7244
Carlos C <uldericofilho@gmail.com>
2017-01-25 23:59:34 -0200

    simple otp tool

---
+++ otp/README.md
@@ -0,0 +1,3 @@
+go get [-u] cirello.io/otp
+
+http://godoc.org/cirello.io/otp
---
+++ otp/main.go
@@ -0,0 +1,296 @@
+package main // import "cirello.io/otp"
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha1"
+	"crypto/x509"
+	"database/sql"
+	"encoding/pem"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/user"
+	"path/filepath"
+	"runtime"
+	"runtime/pprof"
+	"runtime/trace"
+	"strings"
+	"text/tabwriter"
+	"time"
+
+	otp "github.com/hgfischer/go-otp"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/urfave/cli"
+)
+
+var homeDir string
+
+func init() {
+	log.SetPrefix("")
+	log.SetFlags(0)
+
+	usr, err := user.Current()
+	if err != nil {
+		log.Fatal(err)
+	}
+	homeDir = usr.HomeDir
+}
+
+func main() {
+	if os.Getenv("OTP_TRACE") != "" {
+		w, err := os.Create("otp.trace")
+		if err != nil {
+			log.Fatal("cannot create application tracing:", err)
+		}
+		err = trace.Start(w)
+		if err != nil {
+			log.Fatal("cannot start application tracing:", err)
+		}
+		defer trace.Stop()
+
+		fcpu, err := os.Create("otp.cpuprofile")
+		if err != nil {
+			log.Fatal("could not create CPU profile: ", err)
+		}
+		if err := pprof.StartCPUProfile(fcpu); err != nil {
+			log.Fatal("could not start CPU profile: ", err)
+		}
+		defer pprof.StopCPUProfile()
+	}
+
+	app := cli.NewApp()
+	app.Version = "1.0.0"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:   "db",
+			Value:  filepath.Join(homeDir, ".ssh", "auth.db"),
+			EnvVar: "OTP_DB",
+		},
+		cli.StringFlag{
+			Name:   "private-key",
+			Value:  filepath.Join(homeDir, ".ssh", "id_rsa"),
+			EnvVar: "OTP_PRIVKEY",
+		},
+	}
+	app.Commands = []cli.Command{
+		initdb(),
+		add(),
+		get(),
+		list(),
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatalf("error: %v", err)
+	}
+
+	if os.Getenv("OTP_TRACE") != "" {
+		fmem, err := os.Create("otp.memprofile")
+		if err != nil {
+			log.Fatal("could not create memory profile: ", err)
+		}
+		runtime.GC() // get up-to-date statistics
+		if err := pprof.WriteHeapProfile(fmem); err != nil {
+			log.Fatal("could not write memory profile: ", err)
+		}
+		fmem.Close()
+	}
+}
+
+func initdb() cli.Command {
+	return cli.Command{
+		Name:  "init",
+		Usage: "initialize the OTP database",
+		Action: func(c *cli.Context) error {
+			db, err := sql.Open("sqlite3", c.GlobalString("db"))
+			if err != nil {
+				return err
+			}
+			defer db.Close()
+
+			queries := []string{
+				"CREATE TABLE IF NOT EXISTS `otps` (`id` INTEGER PRIMARY KEY, `account` char, `issuer` char, `password` blob);",
+				"CREATE UNIQUE INDEX `otps_account_issuer` ON `otps`(`account`, `issuer`);",
+			}
+
+			for _, q := range queries {
+				_, err = db.Exec(q)
+				if err != nil {
+					return err
+				}
+			}
+
+			log.Println("database initialized")
+			return nil
+		},
+	}
+}
+
+func add() cli.Command {
+	return cli.Command{
+		Name:      "add",
+		Usage:     "a new OTP key",
+		ArgsUsage: "`secret` `issuer` `account-name`",
+		Action: func(c *cli.Context) error {
+			priv, err := privkeyfile(c.GlobalString("private-key"))
+			if err != nil {
+				return err
+			}
+
+			secretkey := c.Args().Get(0)
+			issuer := c.Args().Get(1)
+			account := c.Args().Get(2)
+
+			switch {
+			case secretkey == "":
+				return errors.New("secret key is missing")
+			case issuer == "":
+				return errors.New("issuer is missing")
+			case account == "":
+				return errors.New("account name is missing")
+			}
+
+			enckey, err := priv.encrypted([]byte(secretkey), cryptlabel(account, issuer))
+			if err != nil {
+				return err
+			}
+
+			db, err := sql.Open("sqlite3", c.GlobalString("db"))
+			if err != nil {
+				return err
+			}
+			defer db.Close()
+
+			_, err = db.Exec("REPLACE INTO `otps` (`issuer`, `account`, `password`) VALUES (?, ?, ?);", issuer, account, enckey)
+			return err
+		},
+	}
+}
+
+func get() cli.Command {
+	return cli.Command{
+		Name:  "get",
+		Usage: "generate OTP",
+		Action: func(c *cli.Context) error {
+			priv, err := privkeyfile(c.GlobalString("private-key"))
+			if err != nil {
+				return err
+			}
+
+			db, err := sql.Open("sqlite3", c.GlobalString("db"))
+			if err != nil {
+				return err
+			}
+			defer db.Close()
+
+			rows, err := db.Query("SELECT `account`, `issuer`, `password` FROM `otps` ORDER BY `account` ASC, `issuer` ASC;")
+			if err != nil {
+				return err
+			}
+			defer rows.Close()
+
+			w := tabwriter.NewWriter(os.Stdout, 8, 8, 2, ' ', 0)
+			defer w.Flush()
+			fmt.Fprintln(w, "account\tissuer\texpiration\tcode")
+
+			for rows.Next() {
+				var account, issuer string
+				var pw []byte
+				rows.Scan(&account, &issuer, &pw)
+
+				decrypted, err := priv.decrypted(pw, cryptlabel(account, issuer))
+				if err != nil {
+					return err
+				}
+
+				key := strings.ToUpper(strings.Replace(string(decrypted), " ", "", -1))
+				totp := &otp.TOTP{Secret: key, IsBase32Secret: true}
+				token := totp.Get()
+
+				fmt.Fprintln(
+					w,
+					fmt.Sprintf("%s\t%s\t%vs\t%s",
+						issuer,
+						account,
+						(30-time.Now().Unix()%30),
+						token),
+				)
+			}
+
+			return nil
+		},
+	}
+}
+
+func list() cli.Command {
+	return cli.Command{
+		Name:  "list",
+		Usage: "list all keys",
+		Action: func(c *cli.Context) error {
+			db, err := sql.Open("sqlite3", c.GlobalString("db"))
+			if err != nil {
+				return err
+			}
+			defer db.Close()
+
+			rows, err := db.Query("SELECT account, issuer FROM `otps` ORDER BY account ASC, issuer ASC;")
+			if err != nil {
+				return err
+			}
+			defer rows.Close()
+
+			w := tabwriter.NewWriter(os.Stdout, 8, 8, 2, ' ', 0)
+			defer w.Flush()
+			fmt.Fprintln(w, "account\tissuer")
+
+			for rows.Next() {
+				var account, issuer string
+				rows.Scan(&account, &issuer)
+				fmt.Fprintln(w, fmt.Sprintf("%s\t%s", issuer, account))
+			}
+
+			return nil
+		},
+	}
+}
+
+type privkey struct {
+	*rsa.PrivateKey
+}
+
+func privkeyfile(fn string) (*privkey, error) {
+	pemdata, err := ioutil.ReadFile(fn)
+	if err != nil {
+		return nil, fmt.Errorf("cannot read key file: %s", err)
+	}
+
+	block, _ := pem.Decode(pemdata)
+	if block == nil {
+		return nil, errors.New("key data is not PEM encoded")
+	}
+
+	if got, want := block.Type, "RSA PRIVATE KEY"; got != want {
+		return nil, fmt.Errorf("mismatched key type. got: %q want: %q", got, want)
+	}
+
+	priv, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		return nil, fmt.Errorf("invalid private key: %s", err)
+	}
+
+	return &privkey{PrivateKey: priv}, nil
+}
+
+func (p privkey) encrypted(in, label []byte) ([]byte, error) {
+	return rsa.EncryptOAEP(sha1.New(), rand.Reader, &p.PublicKey, in, label)
+}
+
+func (p privkey) decrypted(in, label []byte) ([]byte, error) {
+	return rsa.DecryptOAEP(sha1.New(), rand.Reader, p.PrivateKey, in, label)
+}
+
+func cryptlabel(account, issuer string) []byte {
+	return []byte(fmt.Sprint(account, issuer))
+}
