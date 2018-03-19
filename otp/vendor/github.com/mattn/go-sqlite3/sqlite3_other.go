2952b63905f506c02be10162daf9149877c579a3
Carlos C <uldericofilho@gmail.com>
2018-03-19 23:39:31 +0100

    Update dependencies

--- otp/vendor/github.com/mattn/go-sqlite3/sqlite3_other.go
+++ otp/vendor/github.com/mattn/go-sqlite3/sqlite3_other.go
@@ -1,3 +1,7 @@
+// Copyright (C) 2014 Yasuhiro Matsumoto <mattn.jp@gmail.com>.
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
 // +build !windows
 
 package sqlite3
@@ -5,5 +9,6 @@ package sqlite3
 /*
 #cgo CFLAGS: -I.
 #cgo linux LDFLAGS: -ldl
+#cgo solaris LDFLAGS: -lc
 */
 import "C"
