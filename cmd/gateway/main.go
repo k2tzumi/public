432a10c638afc0a514db167f8fe360ea270edf38
Carlos C <uldericofilho@gmail.com>
2018-07-01 01:33:49 +0200

    Add gateway service

---
+++ cmd/gateway/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"os"
+	"sync"
+)
+
+const gatewayTokenCookie = "gateway-jwt"
+
+var (
+	publicBindIP   = os.Getenv("GATEWAY_PUBLIC_BIND_IP")
+	servicesBindIP = os.Getenv("GATEWAY_SERVICES_BIND_IP")
+	googleClientID = os.Getenv("GATEWAY_GOOGLE_CLIENT_ID")
+	links          = os.Getenv("GATEWAY_LINKS_GIST")
+	baseGithubAcct = os.Getenv("GATEWAY_BASE_GITHUB_ACCOUNT")
+	frontPkgDomain = os.Getenv("GATEWAY_FRONT_PACKAGE_DOMAIN")
+)
+
+func main() {
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		services()
+	}()
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		publicSites()
+	}()
+	wg.Wait()
+}
