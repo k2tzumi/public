954e1872171ab4a5339c6b4df976f058c4666437
Carlos C <uldericofilho@gmail.com>
2018-07-08 16:32:41 +0200

    ci: automate mirror updates

--- scripts/export-mirrors.sh
+++ scripts/export-mirrors.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-git remote | grep -v -E "origin|_" | while read line; do ./scripts/push-repo.sh $line; done;
+export REPOS=(HumorChecker dynamolock errors goherokuname runner supervisor)
+
+for repo in $REPOS
+do
+	./scripts/push-repo.sh $repo
+done
 
