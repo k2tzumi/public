8f49a4d042d43db2377bf2a62905efc4263538e4
Ulderico Cirello <u@cirello.eu>
2018-07-26 00:57:46 +0200

    exp/sdci: store build logs in the database

--- exp/sdci/pkg/coordinator/coordinator.go
+++ exp/sdci/pkg/coordinator/coordinator.go
@@ -1,5 +1,7 @@
 package coordinator // import "cirello.io/exp/sdci/pkg/coordinator"
 import (
+	"time"
+
 	"cirello.io/errors"
 	"github.com/jmoiron/sqlx"
 )
@@ -14,10 +16,15 @@ type Recipe struct {
 
 // Build defines the necessary data to run a build successfully.
 type Build struct {
-	ID            int64  `db:"id"`
-	RepoFullName  string `db:"repo_full_name"`
-	CommitHash    string `db:"commit_hash"`
-	CommitMessage string `db:"commit_message"`
+	ID            int64     `db:"id"`
+	RepoFullName  string    `db:"repo_full_name"`
+	CommitHash    string    `db:"commit_hash"`
+	CommitMessage string    `db:"commit_message"`
+	InProgress    bool      `db:"in_progress"`
+	StartedAt     time.Time `db:"started_at"`
+	Success       bool      `db:"success"`
+	Log           string    `db:"log"`
+	CompletedAt   time.Time `db:"completed_at"`
 	*Recipe
 }
 
@@ -50,7 +57,12 @@ func (c *Coordinator) bootstrap() {
 			commit_hash text,
 			commit_message text,
 			environment text,
-			commands text
+			commands text,
+			started_at bigint,
+			in_progress bool,
+			success bool,
+			log bigtext,
+			completed_at bigint
 		);
 	`)
 	if err != nil {
@@ -96,4 +108,33 @@ func (c *Coordinator) Enqueue(b *Build) {
 // Next returns the next job in the pipe. If nil, the client must stop reading.
 func (c *Coordinator) Next() *Build {
 	return <-c.out
+}
+
+// MarkInProgress determines a build has started and update its build
+// information in the database.
+func (c *Coordinator) MarkInProgress(b *Build) error {
+	b.StartedAt = time.Now()
+	_, err := c.db.NamedExec(`
+	UPDATE builds
+		SET in_progress = true, started_at = :started_at
+		WHERE id = :id
+	`, b)
+	return errors.E(err, "cannot set job to in-progress")
+}
+
+// MarkComplete determines a build has completed and update its build
+// information in the database
+func (c *Coordinator) MarkComplete(b *Build) error {
+	b.CompletedAt = time.Now()
+	b.InProgress = false
+	_, err := c.db.NamedExec(`
+		UPDATE builds
+		SET
+			in_progress = false,
+			success = :success,
+			log = :log,
+			completed_at = :completed_at
+		WHERE id = :id
+	`, b)
+	return errors.E(err, "cannot update job in the database")
 }
