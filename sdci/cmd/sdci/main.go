b881eb3f696adf62def5ade68bc44735459c5930
Ulderico Cirello <u@cirello.eu>
2018-07-28 16:26:22 +0200

    exp/sdci: rearchitecture CLI to allow splitting server and workers.

--- sdci/cmd/sdci/main.go
+++ sdci/cmd/sdci/main.go
@@ -1,125 +1,29 @@
 // Command sdci implements a simple and dirty CI service.
 package main // import "cirello.io/exp/sdci/cmd/sdci"
 import (
-	"context"
 	"log"
-	"net"
 	"os"
-	"os/user"
-	"path/filepath"
 
-	"cirello.io/exp/sdci/pkg/coordinator"
-	"cirello.io/exp/sdci/pkg/grpc/api"
-	"cirello.io/exp/sdci/pkg/grpc/server"
-	"cirello.io/exp/sdci/pkg/infra/repositories"
-	"cirello.io/exp/sdci/pkg/models"
-	"cirello.io/exp/sdci/pkg/ui/dashboard"
-	"cirello.io/exp/sdci/pkg/ui/webhooks"
-	"cirello.io/exp/sdci/pkg/worker"
+	"cirello.io/exp/sdci/pkg/ui/cli"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/mattn/go-sqlite3"
-	"google.golang.org/grpc"
 )
 
 func main() {
-	log.SetFlags(0)
 	log.SetPrefix("sdci: ")
-	buildsDir := buildsDir()
-	db := openDB()
-	configuration := loadConfiguration()
-	ctx, coord := startCoordinator(db, configuration)
-	startWebhooksServer(ctx, coord)
-	startDashboard(ctx, db)
-	log.Println("ok")
-	addr := startGRPCServer(ctx, coord, configuration)
-	startWorkers(ctx, addr, buildsDir, coord)
-	if err := coord.Wait(); err != nil {
-		log.Fatalln("coordinator error:", err)
+	log.SetFlags(0)
+	fn := "sdci.db"
+	if envFn := os.Getenv("SDCI_DB"); envFn != "" {
+		fn = envFn
 	}
+	db := openDB(fn)
+	cli.Run(db)
 }
 
-func buildsDir() string {
-	currentUser, err := user.Current()
-	if err != nil {
-		log.Fatalln("cannot load current user information:", err)
-	}
-	return filepath.Join(currentUser.HomeDir, ".sdci", "builds-%v")
-}
-
-func openDB() *sqlx.DB {
-	db, err := sqlx.Open("sqlite3", "sdci.db")
+func openDB(fn string) *sqlx.DB {
+	db, err := sqlx.Open("sqlite3", fn)
 	if err != nil {
 		log.Fatalln("cannot open database:", err)
 	}
 	return db
-}
-
-func loadConfiguration() models.Configuration {
-	fd, err := os.Open("sdci-config.yaml")
-	if err != nil {
-		log.Fatal("cannot open configuration file:", err)
-	}
-	configuration, err := models.LoadConfiguration(fd)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return configuration
-}
-
-func startCoordinator(db *sqlx.DB, configuration models.Configuration) (context.Context, *coordinator.Coordinator) {
-	ctx, coord := coordinator.New(db, configuration)
-	if err := coord.Error(); err != nil {
-		log.Fatalln("coordinator error on start:", err)
-	}
-	return ctx, coord
-}
-
-func startWorkers(ctx context.Context, grpcServerAddr, buildsDir string,
-	coord *coordinator.Coordinator) {
-	err := worker.Start(ctx, grpcServerAddr, buildsDir)
-	if err != nil {
-		log.Fatalln("coordinator error on start:", err)
-	}
-}
-
-func startWebhooksServer(ctx context.Context, coord *coordinator.Coordinator) {
-	webhookListener, err := net.Listen("tcp", ":6500")
-	if err != nil {
-		log.Fatalln("cannot start web server:", err)
-	}
-	webhookServer := webhooks.New(coord)
-	go func() {
-		log.Println(webhookServer.ServeContext(ctx, webhookListener))
-	}()
-}
-
-func startDashboard(ctx context.Context, db *sqlx.DB) {
-	dashboardListener, err := net.Listen("tcp", ":8080")
-	if err != nil {
-		log.Fatalln("cannot start dashboard server:", err)
-	}
-	dashboardServer := dashboard.New(repositories.Builds(db))
-	go func() {
-		if err := dashboardServer.ServeContext(ctx, dashboardListener); err != nil {
-			log.Fatalln("cannot server dashboard:", err)
-		}
-	}()
-}
-
-func startGRPCServer(ctx context.Context, coord *coordinator.Coordinator, configuration models.Configuration) string {
-	l, err := net.Listen("tcp", ":0")
-	if err != nil {
-		log.Fatalln("cannot start dashboard server:", err)
-	}
-	s := server.New(coord, configuration)
-	grpcServer := grpc.NewServer()
-	api.RegisterRunnerServer(grpcServer, s)
-	go func() {
-		err := grpcServer.Serve(l)
-		if err != nil {
-			log.Fatalln("errors in GRPC server:", err)
-		}
-	}()
-
-	return l.Addr().String()
 }
