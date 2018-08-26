c4ea3deb86c2be59290a1e370b47074bc69ce04a
Ulderico Cirello <u@cirello.eu>
2018-08-26 20:12:24 +0200

    exp/mlflappygopher: store round information within AI memory

--- mlflappygopher/ai.go
+++ mlflappygopher/ai.go
@@ -1,11 +1,14 @@
 package main
 
 import (
-	"fmt"
+	"time"
 )
 
 type ai struct {
 	bird *bird
+
+	currentRound *round
+	rounds       []*round
 }
 
 func newAI(b *bird) *ai {
@@ -15,10 +18,27 @@ func newAI(b *bird) *ai {
 }
 
 func (ai *ai) feedInputLayer(dx, dy int32) {
-	fmt.Println("perceived distances", dx, dy)
-	// TODO: trigger window flapping
+	if ai.currentRound == nil {
+		ai.currentRound = &round{start: time.Now()}
+	}
+	// fmt.Println("perceived distances", dx, dy)
+	// TODO: trigger bird flapping.
 	// if rand.Float32() > 0.95 {
 	// 	ai.bird.jump()
 	// 	fmt.Println("flapped the wings")
 	// }
+}
+
+func (ai *ai) iterate() {
+	// TODO: reset neural network and evolve.
+	ai.currentRound.duration = time.Since(ai.currentRound.start)
+	ai.rounds = append(ai.rounds, ai.currentRound)
+	ai.currentRound = nil
+}
+
+type round struct {
+	start    time.Time
+	duration time.Duration
+
+	// TODO: add neural net info.
 }
