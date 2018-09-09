81b652fe8149b3fe2f53297e2678cac0ef127faa
Ulderico Cirello <u@cirello.eu>
2018-09-08 18:19:29 -0700

    exp/mlflappygopher: change the pipes to be pairs of top and bottom pipes

--- mlflappygopher/pipes.go
+++ mlflappygopher/pipes.go
@@ -122,13 +122,12 @@ func (ps *pipes) closestPipe() *pipe {
 	}
 	for _, p := range ps.pipes {
 		p.mu.Lock()
-		x, h, w, inverted := p.x, p.h, p.w, p.inverted
+		x, h, w := p.x, p.h, p.w
 		p.mu.Unlock()
 		return &pipe{
-			x:        x,
-			h:        h,
-			w:        w,
-			inverted: inverted,
+			x: x,
+			h: h,
+			w: w,
 		}
 	}
 	return nil
@@ -137,18 +136,16 @@ func (ps *pipes) closestPipe() *pipe {
 type pipe struct {
 	mu sync.RWMutex
 
-	x        int32
-	h        int32
-	w        int32
-	inverted bool
+	x int32
+	h int32
+	w int32
 }
 
 func newPipe() *pipe {
 	return &pipe{
-		x:        windowWidth,
-		h:        100 + int32(rand.Intn(300)),
-		w:        50,
-		inverted: rand.Float32() > 0.5,
+		x: windowWidth,
+		h: 100 + int32(rand.Intn(300)),
+		w: 50,
 	}
 }
 
@@ -162,15 +159,22 @@ func (p *pipe) paint(r *sdl.Renderer, texture *sdl.Texture) error {
 	p.mu.RLock()
 	defer p.mu.RUnlock()
 
-	rect := &sdl.Rect{X: p.x, Y: windowHeight - p.h, W: p.w, H: p.h}
-	flip := sdl.FLIP_NONE
-	if p.inverted {
-		rect.Y = 0
-		flip = sdl.FLIP_VERTICAL
+	// top pipe
+	{
+		rect := &sdl.Rect{X: p.x, Y: 0, W: p.w, H: windowHeight - p.h - 3*birdHeight}
+		flip := sdl.RendererFlip(sdl.FLIP_VERTICAL)
+		if err := r.CopyEx(texture, nil, rect, 0, nil, flip); err != nil {
+			return fmt.Errorf("could not copy background: %v", err)
+		}
 	}
 
-	if err := r.CopyEx(texture, nil, rect, 0, nil, flip); err != nil {
-		return fmt.Errorf("could not copy background: %v", err)
+	// bottom pipe
+	{
+		rect := &sdl.Rect{X: p.x, Y: windowHeight - p.h, W: p.w, H: p.h}
+		flip := sdl.FLIP_NONE
+		if err := r.CopyEx(texture, nil, rect, 0, nil, flip); err != nil {
+			return fmt.Errorf("could not copy background: %v", err)
+		}
 	}
 
 	return nil
