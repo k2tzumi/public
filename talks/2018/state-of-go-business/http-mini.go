4d07b9e208af7f5095b5d2cf47c558c1fcb13dcc
Ulderico Cirello <u@cirello.eu>
2018-10-10 01:35:46 +0200

    talks: state of go for business - 2018

---
+++ talks/2018/state-of-go-business/http-mini.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "hello world")
+	})
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
