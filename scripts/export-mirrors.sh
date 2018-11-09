b936858b9fcd4d6905c0623aaed4fa20efa224c6
Ulderico Cirello <u@cirello.eu>
2018-11-09 15:48:28 +0100

    scripts: export oversight

--- scripts/export-mirrors.sh
+++ scripts/export-mirrors.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export REPOS="HumorChecker dynamolock errors goherokuname runner supervisor bookmarkd snippetsd cci mlflappygopher kohrah-ani"
+export REPOS="HumorChecker dynamolock errors goherokuname runner supervisor bookmarkd snippetsd cci mlflappygopher kohrah-ani oversight"
 
 for repo in $REPOS
 do
