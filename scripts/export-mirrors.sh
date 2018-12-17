b26093a415aff1a66d3c025a33994415b10cb334
Ulderico Cirello <u@cirello.eu>
2018-12-17 15:35:44 -0200

    pglock: first iteration of row-based postgresql lock client

--- scripts/export-mirrors.sh
+++ scripts/export-mirrors.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export REPOS="HumorChecker dynamolock errors goherokuname runner supervisor bookmarkd snippetsd cci mlflappygopher kohrah-ani oversight"
+export REPOS="HumorChecker dynamolock errors goherokuname runner supervisor bookmarkd snippetsd cci mlflappygopher kohrah-ani oversight pglock"
 
 for repo in $REPOS
 do
