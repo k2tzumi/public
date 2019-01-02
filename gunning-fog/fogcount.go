de05bb75b36fff2b9231575557d2892463c8b0fd
Ulderico Cirello <u@cirello.eu>
2019-01-02 19:17:31 +0100

    all: normalize licenses with Apache 2.0 everywhere

--- gunning-fog/fogcount.go
+++ gunning-fog/fogcount.go
@@ -1,3 +1,17 @@
+// Copyright 2019 github.com/ucirello and https://cirello.io. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to writing, software distributed
+// under the License is distributed on a "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied.
+//
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // Gunning-fog index analyzer written in Go. This analyzer processes an English
 // text and produces its Gunning Fox index score. Refer to its logic in
 // https://en.wikipedia.org/wiki/Gunning_fog_index - it does not analyse word
