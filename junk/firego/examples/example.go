d24422f7180a3d1d5ea7cd2d7a897e538096c784
Ulderico Cirello <u@cirello.eu>
2019-01-27 00:54:07 +0100

    junk/firego: fix import

--- junk/firego/examples/example.go
+++ junk/firego/examples/example.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/dericofilho/firego"
+	firego "cirello.io/junk/FireGo"
 )
 
 type Application struct {
--- junk/firego/firego.go
+++ junk/firego/firego.go
@@ -14,7 +14,7 @@
 // Check the example to see it working:
 //
 // # go run examples/example.go
-package firego // import "cirello.io/FireGo"
+package firego
 
 //go:generate stringer -type=MessageType
 import (
